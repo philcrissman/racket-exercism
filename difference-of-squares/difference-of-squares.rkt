05b194293026565e236e9adec8622b97809d2c9e
Phil Crissman <phcrissm@microsoft.com>
2022-04-05 12:46:20 -0700

    Use foldl instead of reimplementing reduce; use range instead of manually making a list in listof

--- difference-of-squares/difference-of-squares.rkt
+++ difference-of-squares/difference-of-squares.rkt
@@ -6,18 +6,11 @@
   (lambda (n)
     (* n n)))
 
-(define (reduce f n lat)
-    (cond
-      [(empty? (cdr lat)) (f (car lat) n)]
-      [else (reduce f (f n (car lat)) (cdr lat))]))
-
 (define (sum lat)
-  (reduce (lambda (x y)(+ x y)) 0 lat))
+  (foldl + 0 lat))
 
 (define (listof n)
-  (cond
-    [(equal? n 0) '()]
-    [else (cons n (listof (- n 1)))]))
+  (range 1 (+ n 1)))
 
 (define (square-of-sum n)
   (square (sum (listof n))))
